020f4c1075d42db9402a2c2c3e6634b67e7aea33
dtxue-95 <dtjqxue@163.com>
2025-06-25 08:46:44 +0800

    feat: 修改

--- src/lib/ReactMindMap.tsx
+++ src/lib/ReactMindMap.tsx
@@ -83,7 +83,7 @@ export default function ReactMindMap({
   showTopToolbar = true,
   showBottomToolbar = true,
   readOnly = true,
-  onDataChange,
+  // onDataChange,
   topToolbarCustomButtons,
   bottomToolbarCustomButtons,
   getNodeStyle,
